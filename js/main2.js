2c5c0cace636ea3cc35b70519e8e3372439c6539
Lilianeth Diaz QA <lilianethqa@gmail.com>
2024-08-17 21:10:21 -0400

    Continuacion ejercicio 06B

---
+++ js/main2.js
@@ -0,0 +1,12 @@
+var cantidadDeGatos = 5;
+    var cantidadDePasos = 3;
+    
+    for (var i = 1; i <= cantidadDeGatos; i++) {
+        var pasos = '';
+        
+        for (var j = 0; j < cantidadDePasos; j++) {
+            pasos += '🐾';
+        }
+        
+    console.log("Gato #" + i + ": 🐈" + pasos);
+    }
