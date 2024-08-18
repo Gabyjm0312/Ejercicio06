3c79675a683288935c082b612deb0180818f9a9b
Gabyjm0312 <gabyjm0312@gmail.com>
2024-08-17 20:43:52 -0400

    Primer ejercicio realizado

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var cantidadGatos = 10;
+
+for (var i = 1; i <= cantidadGatos; i++) {
+
+    var gato1 = '😺'
+    var gato2 = '😸'
+    var gato3 = '😹'
+
+    if(i == 1 ){console.log('Gato #' + i + gato1 )}
+    else if(i == 2 ){console.log('Gato #' + i , gato2 )}
+    else if(i == 3 ){console.log('Gato #' + i , gato3 )}
+    else if(i == 4 ){console.log('Gato #' + i , gato1 )}
+    else if(i == 5 ){console.log('Gato #' + i , gato2 )}
+    else if(i == 6 ){console.log('Gato #' + i , gato3 )}
+    else if(i == 7 ){console.log('Gato #' + i , gato1 )}
+    else if(i == 8 ){console.log('Gato #' + i , gato2 )}
+    else if(i == 9 ){console.log('Gato #' + i , gato3 )}
+    else if(i == 10 ){console.log('Gato #' + i , gato1 )} 
+    
+}
